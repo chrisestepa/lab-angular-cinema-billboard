35670eb30710818adedb680cba1fa111314cad43
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-27 18:28:52 +0200

    segunda iteraccion ok

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -2,12 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { Routes } from "@angular/router";
 import { AppComponent } from './app.component';
+import { MyHomeComponentComponent } from './my-home-component/my-home-component.component';
+import { MyMovieComponentComponent } from './my-movie-component/my-movie-component.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  component: MyHomeComponent },
+  { path: 'movie/:id', component: MyMovieComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHomeComponentComponent,
+    MyMovieComponentComponent
   ],
   imports: [
     BrowserModule,
